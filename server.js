8db823af3c2de2b6c1597d56bbdc99f84d06bde0
nicolezeg <nzegarra07@gmail.com>
2026-08-01 22:52:12 -0500

    controller pattern

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
-import { getAllOrganizations } from './src/models/organizations.js';
-import { getAllProjects } from './src/models/projects.js';
-import { getAllCategories } from './src/models/categories.js';
 import express from 'express';
 import { fileURLToPath } from 'url';
 import path from 'path';
 import { testConnection } from './src/models/db.js';
+import router from './src/routes.js';
 
 // Define the application environment
 const NODE_ENV = process.env.NODE_ENV?.toLowerCase() || 'production';
@@ -44,43 +42,7 @@ app.use((req, res, next) => {
     next();
 });
 
-/**
-  * Routes
-  */
-app.get('/', async (req, res) => {
-    const title = 'Home';
-    res.render('home', { title });
-});
-
-app.get('/organizations', async (req, res) => {
-    const organizations = await getAllOrganizations();    
-    const title = 'Our Partner Organizations';
-
-    res.render('organizations', { title, organizations });
-});
-
-app.get('/projects', async (req, res) => {
-    const projects = await getAllProjects();
-    const title = 'Service Projects';
-
-    res.render('projects', { title, projects });
-});
-
-app.get('/categories', async (req, res) => {
-    const categories = await getAllCategories();
-
-    res.render('categories', {
-        title: 'Categories',
-        categories
-    });
-});
-
-// Test route for 500 errors
-app.get('/test-error', (req, res, next) => {
-    const err = new Error('This is a test error');
-    err.status = 500;
-    next(err);
-});
+app.use(router);
 
 // Catch-all route for 404 errors
 app.use((req, res, next) => {
@@ -113,10 +75,6 @@ app.use((err, req, res, next) => {
 app.listen(PORT, async () => {
   try {
     await testConnection();
-
-    const projects = await getAllProjects();
-    console.log(projects);
-
     console.log(`Server is running at http://127.0.0.1:${PORT}`);
     console.log(`Environment: ${NODE_ENV}`);
   } catch (error) {
