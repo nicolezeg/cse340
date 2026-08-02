809c51a1d1d156625bbc5481de052af4d85553ff
nicolezeg <nzegarra07@gmail.com>
2026-08-01 23:39:41 -0500

    activitites w2

--- src/models/projects.js
+++ src/models/projects.js
@@ -1,6 +1,6 @@
 import db from './db.js';
 
-export async function getAllProjects() {
+const getAllProjects = async() => {
     const query = `
         SELECT 
             service_project.project_id,
@@ -18,3 +18,26 @@ export async function getAllProjects() {
 
     return result.rows;
 }
+
+const getProjectsByOrganizationId = async (organizationId) => {
+      const query = `
+        SELECT
+          project_id,
+          organization_id,
+          title,
+          description,
+          location,
+          date
+        FROM service_project
+        WHERE organization_id = $1
+        ORDER BY date;
+      `;
+      
+      const queryParams = [organizationId];
+      const result = await db.query(query, queryParams);
+
+      return result.rows;
+};
+
+// Export the model functions
+export { getAllProjects, getProjectsByOrganizationId };
